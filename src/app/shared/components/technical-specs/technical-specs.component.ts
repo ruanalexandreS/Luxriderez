c2f24fb39d7d3570f3adbb1a1f89fa9aeb38b5b6
ruanalexandreS <ruancampos366@gmail.com>
2026-02-26 18:37:04 -0300

    fix: alinhando seletores e classes do componente técnico

---
+++ src/app/shared/components/technical-specs/technical-specs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-specs',
+  standalone: true,
+  imports: [],
+  templateUrl: './technical-specs.component.html',
+  styleUrl: './technical-specs.component.css',
+})
+export class TechnicalSpecsComponent {}

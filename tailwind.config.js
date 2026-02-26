98b7b9388fb3e11ebecda0ecf1c09823df4f8ced
ruanalexandreS <ruancampos366@gmail.com>
2026-02-26 17:51:41 -0300

    feat: configuração manual do tailwind e estrutura base

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+@type {import('tailwindcss').Config}
+module.exports = {
+content: [
+    "./src/**/*.{html,ts}",
+],
+theme: {
+    extend: {
+    colors: {
+        'lux-red': '#ff0000',
+        'lux-dark': '#121212',
+    }
+    },
+},
+plugins: [],
+}
